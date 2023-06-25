2624e24f47918c5cb23e76e31e12cfe85226d40c
Alberto G <albertoguedesesilva@outlook.com>
2023-06-25 09:11:51 -0300

    Added hide navMenu function when links click

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,16 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 };
+
+// Esconde o navMenu caso clique em algum link ou fora do menu
+const navLinks = document.querySelectorAll(".nav__link, .nav__link-button"),
+  main = document.querySelector("main")
+
+const menuHiddenFunction = () => {
+  const navMenu = document.getElementById("nav-menu")
+  navMenu.classList.remove("show-menu")
+}
+
+navLinks.forEach(link => {
+  link.addEventListener("click", menuHiddenFunction)
+});
